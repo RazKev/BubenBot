b2cf941614a70d4e341e2a2d329200471f6f639a
Kevin Leirich <kevin.leirich@gmail.com>
2020-06-02 21:52:14 +0200

    Added TagModule

---
+++ BubenBot.Bot/Modules/TagModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BubenBot.Services.Tag;
+using Discord.Commands;
+
+namespace BubenBot.Bot.Modules
+{
+    [Name("Tags")]
+    [Group("tag")]
+    [Alias("tags")]
+    public class TagModule : ModuleBase
+    {
+        private readonly ITagService _tagService;
+
+        public TagModule(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        [Command("create")]
+        [Alias("add")]
+        public async Task CreateTagAsync(string name, [Remainder] string content)
+        {
+            await _tagService.CreateTagAsync(Context.Guild.Id, Context.User.Id, name, content);
+        }
+    }
+}
