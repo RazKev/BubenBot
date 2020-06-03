54098387276123c8e4cb2ab637040cff6a18a3c1
Kevin Leirich <kevin.leirich@gmail.com>
2020-06-03 22:45:35 +0200

    Register commands in StartupService

--- BubenBot.Services/StartupService.cs
+++ BubenBot.Services/StartupService.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -12,21 +15,28 @@ namespace BubenBot.Services
     public class StartupService : IHostedService
     {
         private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
         private readonly IConfiguration _configuration;
+        private readonly IServiceScopeFactory _scopeFactory;
         private readonly ILogger _logger;
 
         public StartupService(
             DiscordSocketClient client,
+            CommandService commands,
             IConfiguration configuration,
+            IServiceScopeFactory scopeFactory,
             ILogger<StartupService> logger)
         {
             _client = client;
+            _commands = commands;
             _configuration = configuration;
+            _scopeFactory = scopeFactory;
             _logger = logger;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
+            using var serviceScope = _scopeFactory.CreateScope();
             var token = _configuration["Discord:Token"];
 
             if (string.IsNullOrWhiteSpace(token))
@@ -37,6 +47,8 @@ namespace BubenBot.Services
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
+
+            await _commands.AddModulesAsync(Assembly.GetExecutingAssembly(), serviceScope.ServiceProvider);
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
